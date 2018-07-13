421b70b690bacab55450772d3c0c57e7f5035525
zengzhipeng@yingu.com <zengzhipeng@yingu.com>
2018-07-13 17:56:28 +0800

    mongodb日期格式化

--- src/routes/article.js
+++ src/routes/article.js
@@ -19,6 +19,7 @@ module.exports = function (app) {
 			    assert.equal(1, result.result.n);
 			    assert.equal(1, result.ops.length);
 			    res.send({});
+			    pool.release(db);
 			  });
 		  })
 		  .catch(function(err) {
@@ -57,10 +58,66 @@ module.exports = function (app) {
 			const resourcePromise = pool.acquire();
 			resourcePromise.then(function(db) {
 			    var dbo = db.db("silencegarden");
-				dbo.collection("article").find({ _id: ObjectId(id) }).toArray(function(err, result) { // 返回集合中所有数据
+
+			    console.log('聚合开始--------------------------------')
+			    try { 
+			    	dbo.collection("article").aggregate([ 
+			    		{ $match: { '_id': ObjectId(id) } },
+			    		{	
+
+			    			$project: {
+			    				time: 
+			    					{ 
+				    					$dateToString: 
+				    						{ 
+				    							format: "%Y-%m-%d %H:%M:%S", 
+				    							date: "$time",
+				    							timezone: "+08"
+				    						} 
+			    					},
+			    				title: 1,
+			    				tags: 1,
+			    				html: 1
+			    			}
+			    		}
+			    	], function (err, result) {
+			    		//pool.release(db);
+			    		console.log('释放连接llllllllllllllllllllll')
+			    		if (err) {
+			    			console.log(err)
+			    			console.log('聚合异常xxxxxxxxxxxxxxxxxxxxxxx')
+			    			res.send(resultWrap({}, '系统异常，请稍后再试out', false))
+			    		}
+			    		result.toArray(function (err, result) {
+				    			console.log('----------------------')
+						        if (err) {
+						        	console.log(err)
+						        	res.send(resultWrap({}, '系统异常，请稍后再试inner', false))
+						        } else {
+						        	//console.log(result);
+						        	if (result.length>0) {
+						        		res.send(resultWrap(result[0]))
+						        	} else {
+						        		res.send(resultWrap({}, '该文章未收录！'))
+						        	}
+						        }
+						        pool.release(db);
+							}) 
+
+			    	})
+			    } catch (err) {
+			    	console.log(err)
+					res.send(resultWrap({}, '系统异常，请稍后再试out', false))
+			    } 
+			    console.log('聚合结束================================') 
+			})
+				/*dbo.collection("article").find({ _id: ObjectId(id) }, { 
+						html: 1, title: 1, tags: 1, date: 1 
+					}).toArray(function(err, result) { // 返回集合中所有数据
 			        console.log('----------------------')
 			        if (err) {
-			        	res.send(resultWrap({}, '系统异常，请稍后再试', false))
+			        	console.log(err)
+			        	res.send(resultWrap({}, '系统异常，请稍后再试inner', false))
 			        } else{
 			        	console.log(result);
 			        	if (result.length>0) {
@@ -74,8 +131,8 @@ module.exports = function (app) {
 			  })
 			  .catch(function(err) {
 			  	console.log(err)
-			    res.send(resultWrap({}, '系统异常，请稍后再试', false))
-			  });
+			    res.send(resultWrap({}, '系统异常，请稍后再试out', false))
+			  });*/
 			
 		} else {
 			res.send({});
